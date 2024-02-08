3100b0778d59e4a9db72a7056ba531bc71dfd308
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-02-09 00:12:58 +0100

    My commit

--- Age.py
+++ Age.py
@@ -1,3 +1,10 @@
 age = eval(input("Enter age: "))
 
-
+if (age >= 1) and (age <= 18):
+    print("Important Birthday")
+elif (age == 21) or (age == 50):
+    print("Important Birthday")
+elif not(age < 65):
+    print("Important Birthday")
+else:
+    print("Sorry not Important Birthday")
---
+++ class.py
@@ -0,0 +1,12 @@
+age = eval(input("Enter age: "))
+if age == 5:
+    print("Go to kindergarten")
+for i in range(6,18):
+    if age == i:
+        print("Go to grades 1 through 12")
+        
+if age => 18:
+    print("Go to college")
+else:
+    print("Sorry! wait till you're old enough")    
+
