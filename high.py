90572da1050fb37f9f0b913906b475043f0ee134
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-02-15 21:15:55 +0100

    My commit

--- high.py
+++ high.py
@@ -1,8 +1,4 @@
 import calendar
-name = input("Name: ")
-money = input("Money invested: ")
-interest_rate = input("Interest Rate (%): ")
-
 
 class HighlightCalendar(calendar.TextCalendar):
     def __init__(self, highlight_day):
@@ -15,16 +11,8 @@ class HighlightCalendar(calendar.TextCalendar):
         else:
             return super().formatday(day, weekday, width)
 
-highlight_day = int(input("Enter checkout day: "))
+# Example usage
+highlight_day = int(input("Enter a day: "))
 month = int(input("Enter the month: "))
 cal = HighlightCalendar(highlight_day)
-
-money = float(money)
-
-interest_rate = float(interest_rate) *.01
-n = int(input("Duration: "))
-for i in range (n):
-    money = money + (money * interest_rate)
-    
-print(f"Hi {name}, your ROI after 3 weeks is {money :.2f} Naira only, for the date in the calendar below.")
 cal.prmonth(2024, month)
